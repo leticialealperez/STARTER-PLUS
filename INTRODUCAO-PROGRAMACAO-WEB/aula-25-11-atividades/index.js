0571ad2a1bacf00a72ee93cfd124b2757b2c6518
Leticia Leal <leticia.leal@growdev.com.br>
2022-11-25 16:03:26 -0300

    adiciona código de exemplo para atividades

---
+++ INTRODUCAO-PROGRAMACAO-WEB/aula-25-11-atividades/index.js
@@ -0,0 +1,38 @@
+// DADOS - input
+let numero = 30;
+let percentual = 50;
+
+
+// manipulação de dados - aritmetica, comparacao, atribuição...
+let resultado =  30 * percentual  / 100;
+
+
+// informacao - SAIDA DA INFORMAÇÃO
+console.log(`${percentual} de ${numero} é ${resultado}`);
+
+
+
+
+
+
+
+
+
+
+// CONSOLE
+/* const idade = 18;
+console.log("Frase de exemplo") // concatenação de string utilizando o operador + 
+
+// template string - UTILIZAÇÃO DA CRASE PARA A CONSTRUÇÃO DE FRASES CONCATENADAS COM VARIAVEIS
+console.log(`A minha idade é ${idade}`)
+ */
+
+
+// WINDOW
+/* window.alert('Frase de exemplo'); */ // ele congela a guia que esta sendo executada no navegador
+/* console.log('Executou aqui'); */
+
+
+// DOCUMENTO HTML
+/* const idade = 18;
+document.write(`<h2> Minha idade é ${idade} </h2>`); */
