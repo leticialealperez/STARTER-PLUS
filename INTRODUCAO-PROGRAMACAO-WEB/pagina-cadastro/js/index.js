ac7df6bbdf700d04a96107cd2e447bfa7b9c1bd6
Leticia Leal <leticia.leal@growdev.com.br>
2023-01-09 10:08:46 -0300

    adiciona comentários dos códigos implementados

--- INTRODUCAO-PROGRAMACAO-WEB/pagina-cadastro/js/index.js
+++ INTRODUCAO-PROGRAMACAO-WEB/pagina-cadastro/js/index.js
@@ -24,33 +24,50 @@ formularioCadastroHTML.addEventListener('submit', (ev) => {
     // previne o comportamento padrão do submit - remove o refresh
     ev.preventDefault()
 
+    // captura do elemento na DOM
     const inputEmail = window.document.getElementById('email')
+    
+    // adiciona um observador, para verificar quando damos foco no input (cursor piscando no input)
+    // quando isso acontecer, o javascript executará a função passada após a virgula
+    // ocultará o feedback de erro mostrado ao usuário
     inputEmail.addEventListener('focus', () => {
         feedbackHTML.innerHTML = ''
     })
-    console.log(inputEmail.value)
+    // console.log(inputEmail.value)
 
 
+    // captura do elemento na DOM
     const inputPassword = window.document.getElementById('password')
+
+    // adiciona um observador, para verificar quando damos foco no input (cursor piscando no input)
+    // quando isso acontecer, o javascript executará a função passada após a virgula
+    // ocultará o feedback de erro mostrado ao usuário
     inputPassword.addEventListener('focus', () => {
         feedbackHTML.innerHTML = ''
     })
-    console.log(inputPassword.value)
+    // console.log(inputPassword.value)
 
 
+    // captura do elemento na DOM
     const inputRepassword = window.document.getElementById('repassword')
+
+    // adiciona um observador, para verificar quando damos foco no input (cursor piscando no input)
+    // quando isso acontecer, o javascript executará a função passada após a virgula
+    // ocultará o feedback de erro mostrado ao usuário
     inputRepassword.addEventListener('focus', () => {
         feedbackHTML.innerHTML = ''
     })
-    console.log(inputRepassword.value)
+    // console.log(inputRepassword.value)
 
 
+    // captura do elemento na DOM onde aparecerá o feedback de erro ao usuário
     const feedbackHTML = window.document.getElementById('feedback')
 
 
     // verificar se o que o usuario digitou em password é igual ao que ele digitou em repassword
     if(inputPassword.value !== inputRepassword.value) {
         
+        // mostra o conteúdo de erro ao usuário
         feedbackHTML.innerHTML = 'As senhas não conferem! 🥲'
 
         return
@@ -60,13 +77,17 @@ formularioCadastroHTML.addEventListener('submit', (ev) => {
     // verificar se o usuario já está cadastrado
     const existe = listaUsuarios.some((valor) => valor.email === inputEmail.value)
 
+    // se o valor na variavel existe for "true" não deixa cadastrar e mostra o motivo
     if(existe) {
+
+        // mostra o conteúdo de erro ao usuário
         feedbackHTML.innerHTML = 'Este e-mail já foi cadastrado. Tenta outro aí! 😅'
 
         return
     }
 
 
+    // caso o usuário não exista (valor em "existe" for "false") dá continuidade a lógica de cadastro
     // criar o objeto do novoUsuario
     const novoUsuario = {
         email: inputEmail.value,
@@ -81,7 +102,7 @@ formularioCadastroHTML.addEventListener('submit', (ev) => {
     console.log(listaUsuarios)
 
 
-
+    // limpar os campos de input após cadastro do novo usuário
     inputEmail.value = ''
     inputPassword.value = ''
     inputRepassword.value = ''
